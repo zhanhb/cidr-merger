825962f6976121e04e10838b2367f97af6e08c2c
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-07-04 15:47:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ LDFLAGS="-X main.VERSION=$VERSION -s -w"
 GCFLAGS=""
 GOMIPS=softfloat
 
-env CGO_ENABLED=0 GOOS=linux GOARCH=mips GOMIPS=$GOMIPS go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o cidr-merger sort-ip.go
+env CGO_ENABLED=0 GOOS=linux GOARCH=mips GOMIPS=$GOMIPS go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o cidr-merger
 
 upx --best --ultra-brute cidr-merger
